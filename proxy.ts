71984d92bbc26e06d7655bb44b7545f910941f82
Mkhulisi <mkhulisincube@gmail.com>
2026-03-22 18:23:29 +0200

    38-deployment_test

--- proxy.ts
+++ proxy.ts
@@ -1,26 +1,18 @@
 import NextAuth from 'next-auth'
 import authConfig from './auth.config'
-import { NextResponse } from 'next/server'
-import { i18n } from './i18n-config'
+import createMiddleware from 'next-intl/middleware'
+import { routing } from './i18n/routing'
+
+const intlMiddleware = createMiddleware(routing)
 
 const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
-  const { pathname } = req.nextUrl
-
-  const localeCodes = i18n.locales.map((l) => l.code)
-
-  const pathnameHasLocale = localeCodes.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
-  )
-
-  if (!pathnameHasLocale) {
-    const locale = i18n.defaultLocale
-    req.nextUrl.pathname = `/${locale}${pathname}`
-    return NextResponse.redirect(req.nextUrl)
-  }
+  return intlMiddleware(req)
 })
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|images|icons|.*\\.jpg|.*\\.jpeg|.*\\.png|.*\\.svg|.*\\.gif|.*\\.webp).*)',
+  ],
 }
